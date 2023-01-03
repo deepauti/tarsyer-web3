35bea585b37de54d37d19b092c8e5c149776672b
deepauti <121533875+deepauti@users.noreply.github.com>
2023-01-03 08:01:01 +0530

    Update landing_video.js

--- assets/js/landing_video.js
+++ assets/js/landing_video.js
@@ -6,12 +6,12 @@
     // Check if the device is a mobile or desktop
     if (navigator.userAgent.match(/Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile/i)) {
       // If the device is a mobile, show the mobile image
-      image.src = 'assets/img/mobile.mp4';
+      image.src = 'https://res.cloudinary.com/dt224xdxl/video/upload/v1672685385/tarsyer-web/img/LANDING_VIDEO_MOBILE_pc9wvg.mp4';
       // aboutus.src = 'assets/img/about_us_mobile.png';
       // namegame.src = 'assets/img/name_game_mobile.png';
     } else {
       // If the device is a desktop, show the desktop image
-      image.src = 'assets/img/LANDING_VIDEO_3.mp4';
+      image.src = 'https://res.cloudinary.com/dt224xdxl/video/upload/v1672685392/tarsyer-web/img/LANDING_VIDEO_3_rdyspa.mp4';
       // aboutus.src = 'assets/img/about_us.svg';
       // namegame.src = 'assets/img/name_game.svg';
     }
